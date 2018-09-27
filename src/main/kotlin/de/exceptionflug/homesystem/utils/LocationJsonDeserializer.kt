9373817cd6405325e8fa50a2cc25c753db1c0331
Nico Britze <dietntexperten@gmail.com>
2018-09-27 21:14:07 +0200

    fix typo

--- src/main/kotlin/de/exceptionflug/homesystem/utils/LocationJsonDeserializer.kt
+++ src/main/kotlin/de/exceptionflug/homesystem/utils/LocationJsonDeserializer.kt
@@ -12,7 +12,7 @@ class LocationJsonDeserializer : JsonDeserializer<Location?> {
 
     override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Location? {
         if(json == null) throw IllegalStateException("Cannot deserialize a null location")
-        if(json !is JsonObject) throw IllegalStateException("JsonElement for location deserialization is in a incorrect type")
+        if(json !is JsonObject) throw IllegalStateException("JsonElement for location deserialization is in an incorrect type")
         val world = Bukkit.getWorld(json["world"].asString)
         if(world == null) {
             Bukkit.getLogger().warning("[HomeSystem] Cannot load serialized location $json. World is null.")
